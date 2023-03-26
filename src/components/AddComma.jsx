497d8ff1ab07ee7e13e5fb0065d65ecf05dad047
hyunsu.An <ahs39490215@gmail.com>
2023-03-26 19:18:53 +0900

    마지막 쉼표 제거, 아이템 개수 추가

--- src/components/AddComma.jsx
+++ src/components/AddComma.jsx
@@ -4,7 +4,7 @@ import "./AddComma.css";
 export default function AddComma() {
   const [state, setState] = useState("");
   const [result, setResult] = useState("");
-
+  const [cnt, setCnt] = useState(0);
   const onChange = (e) => {
     setState(e.target.value);
   };
@@ -14,14 +14,23 @@ export default function AddComma() {
     if (e.nativeEvent.submitter.innerText === "쉼표 추가") {
       const result = state
         .split(" ")
-        .map((word) => `${word},`)
-        .join(" ");
+        .map((word, _, arr) => {
+          setCnt(arr.length);
+          return `${word},`;
+        })
+        .join(" ")
+        .slice(0, -1);
+
       setResult(result);
       setState("");
     } else if (e.nativeEvent.submitter.innerText === "중복제거 쉼표 추가") {
       const result = [...new Set(state.split(" "))]
-        .map((word) => `${word},`)
-        .join(" ");
+        .map((word, _, arr) => {
+          setCnt(arr.length);
+          return `${word},`;
+        })
+        .join(" ")
+        .slice(0, -1);
       setResult(result);
       setState("");
     }
@@ -36,6 +45,7 @@ export default function AddComma() {
         <button>중복제거 쉼표 추가</button>
       </form>
       <p>변환값: {result}</p>
+      <p>아이템 개수: {cnt}</p>
       <CopyToClipboard text={result}>
         <button>변환값 복사</button>
       </CopyToClipboard>
